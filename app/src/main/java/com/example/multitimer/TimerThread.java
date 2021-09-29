24d980bfa7592a83302b4d57889fed199b2d5898
jon <jonandersolk@web,de>
2021-09-29 23:44:31 +0200

    Timer object

--- app/src/main/java/com/example/multitimer/TimerThread.java
+++ app/src/main/java/com/example/multitimer/TimerThread.java
@@ -1,5 +1,8 @@
 package com.example.multitimer;
 
+import static java.lang.System.currentTimeMillis;
+import static java.lang.Thread.sleep;
+
 import java.util.List;
 
 public class TimerThread extends Thread
@@ -17,6 +20,18 @@ public class TimerThread extends Thread
 
     @Override
     public void run() {
-
+        now = currentTimeMillis();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i=0;i<2;i++){
+            if (tlist.get(i).getEndTime()==now)
+            {
+                
+            }
+        }
+        run();
     }
 }
